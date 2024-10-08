908c781e27a2e75c8da63aaa0129d99a5e86df7f
FunnyManBryce <bryce.g.roberts@gmail.com>
2024-10-08 10:22:37 -0700

    multiplayer???

--- Versus Project/Assets/Scripts/Multiplayer/NetworkManagerUI.cs
+++ Versus Project/Assets/Scripts/Multiplayer/NetworkManagerUI.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode.Transports.UTP;
 
 public class NetworkManagerUI : MonoBehaviour
 {
+    public GameObject playersInLobby;
+    public TMP_Text playersInLobbyText;
+    public static int totalPlayers = 0;
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
     public GameObject networkManager;
@@ -16,6 +20,9 @@ public class NetworkManagerUI : MonoBehaviour
        
         hostButton.onClick.AddListener(() => {
             NetworkManager.Singleton.StartHost();
+            playersInLobby.SetActive(true);
+            totalPlayers++;
+            playersInLobbyText.text = "Players in Lobby: " + totalPlayers;
         });
         clientButton.onClick.AddListener(() => {
             NetworkManager.Singleton.StartClient();
