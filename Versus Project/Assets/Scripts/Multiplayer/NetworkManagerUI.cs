59cd9429d0f0ea466952c397d8cba5a6073f7a1c
FunnyManBryce <bryce.g.roberts@gmail.com>
2024-11-01 00:16:57 -0700

    Players will be disconnected if there are too many, and playersInLobby tracks disconnected players!

--- Versus Project/Assets/Scripts/Multiplayer/NetworkManagerUI.cs
+++ Versus Project/Assets/Scripts/Multiplayer/NetworkManagerUI.cs
@@ -8,57 +8,76 @@ using Unity.Netcode.Transports.UTP;
 
 public class NetworkManagerUI : NetworkBehaviour
 {
+    [SerializeField] private NetworkManager networkManagerScript;
     public GameObject playersInLobby;
     public TMP_Text playersInLobbyText;
     public NetworkVariable<int> totalPlayers = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    private int testValue = 0;
+    public NetworkVariable<int> playerMaximum = new NetworkVariable<int>(2, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
+
+    [SerializeField] private GameObject objectHostButton;
+    [SerializeField] private GameObject objectClientButton;
+    [SerializeField] private GameObject IPText;
     public GameObject networkManager;
     public GameObject networkManagerUI;
     
     private void Awake()
     {
-        /*networkManagerUI = GameObject.Find("NetworkManagerUI");
-        Transform UItransform = networkManagerUI.transform;
-        UItransform.GetComponent<NetworkObject>().Spawn(true); */
-        hostButton.onClick.AddListener(() => {
+        hostButton.onClick.AddListener(() => { //Host button creates a lobby and then deactivates all the lobby creating/joining UI
             NetworkManager.Singleton.StartHost();
             playersInLobby.SetActive(true);
-            //LobbyPlayersServerRPC();
+            objectHostButton.SetActive(false);
+            objectClientButton.SetActive(false);
+            IPText.SetActive(false);
         });
-        clientButton.onClick.AddListener(() => {
+        clientButton.onClick.AddListener(() => { //Lobby button joins a lobby and then deactivates all the lobby creating/joining UI
             NetworkManager.Singleton.StartClient();
             playersInLobby.SetActive(true);
-            //LobbyPlayersServerRPC();
+            objectHostButton.SetActive(false);
+            objectClientButton.SetActive(false);
+            IPText.SetActive(false);
         });
     }
 
     public override void OnNetworkSpawn()
     {
-        totalPlayers.OnValueChanged += (int previousValue, int newValue) =>
+        totalPlayers.OnValueChanged += (int previousValue, int newValue) => //Whenever a new player joins a lobby, PlayersInLobby keeps track of it
         {
             playersInLobbyText.text = "Players in Lobby: " + totalPlayers.Value;
             Debug.Log(OwnerClientId + "; total players: " + totalPlayers.Value);
         };
-        LobbyPlayersServerRPC();
+        PlayerJoinedServerRPC();
+        if(totalPlayers.Value >= playerMaximum.Value) //if there are too many players in the lobby, kick out the most recently joined one
+        {
+            DisconnectClientServerRPC(networkManagerScript.LocalClientId);
+        }
     }
 
-    private void Update()
+    private void Start() 
     {
-        
-        //Debug.Log(totalPlayers.Value);
+        networkManagerScript.OnClientDisconnectCallback += PlayerDisconnctedServerRPC; //Makes it so PlayerDisconnectedServerRPC triggers whenever a client disconnects
     }
-
-    public void IPString(string s)
+ 
+    public void IPString(string s) //Sets IP to whatever is typed into the IP box
     {
-        networkManager.GetComponent<UnityTransport>().ConnectionData.Address = s;
+        networkManager.GetComponent<UnityTransport>().ConnectionData.Address = s; 
         Debug.Log(networkManager.GetComponent<UnityTransport>().ConnectionData.Address);
     }
 
-    [Rpc(SendTo.Server)]
-    public void LobbyPlayersServerRPC()
+    [Rpc(SendTo.Server)] //Sends info that a new player has joined to the server. This is then syncronized accross all the clients, since that is how network variables work
+    public void PlayerJoinedServerRPC()
     {
-        totalPlayers.Value++;
+        totalPlayers.Value++;      
+    }
+    [Rpc(SendTo.Server)] //sends info that a client has disconnected to the server. This is then syncronized accross all the clients, since that is how network variables work
+    public void PlayerDisconnctedServerRPC(ulong clientID)
+    {
+        totalPlayers.Value--;
+    }
+    [Rpc(SendTo.Server)] //Disconnects a client based on their clientID
+    public void DisconnectClientServerRPC(ulong clientID)
+    {
+        networkManagerScript.DisconnectClient(clientID);
     }
 }
